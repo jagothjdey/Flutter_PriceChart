00b6294d6e12b6630c0a404ad41113bb9e63fd16
dey <jagothjyotidey91@gmail.com>
2020-08-02 15:57:15 +0200

    Cupertino Button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,16 +13,30 @@ class PriceChartApp extends StatefulWidget {
 }
 
 class _PriceChartApp extends State<PriceChartApp> {
+  var label = 'Hello';
+
+  void buttonPressed() {
+    label = 'button pressed';
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Price Chart App'),
+        appBar: AppBar(
+          title: Text('Price Chart App'),
+        ),
+        body: Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(20),
+          child: CupertinoButton(
+            child: Text(label),
+            color: Colors.lightBlue,
+            padding: EdgeInsets.all(8),
+            onPressed: buttonPressed,
           ),
-          body: Text(
-            'Hello',
-          )),
+        ),
+      ),
     );
   }
 }
